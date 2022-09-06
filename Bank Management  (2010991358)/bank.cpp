d3e1118602ac061fdd3723f9c7ec5aa6c1422c8a
anuj123-45 <anuj1358.be20@chitkara.edu.in>
2022-09-07 02:04:02 +0530

    changes done

--- Bank Management  (2010991358)/bank.cpp
+++ Bank Management  (2010991358)/bank.cpp
@@ -1,18 +1,21 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include <fstream>
 using namespace std;
-
+vector<long double> balance_amount(0,1000);
+int indx=0;
 class customer{
 
+public:
 char depositer_name[50];
  long long int account_number;
 char type_of_acc[10];
-long double balance_amount;
+long double balance;
 
 
 public:
-void addCustomers(){
-  
+void addCustomers(int &i){
+  cout<<endl;
+  cout<<"Enter the details of account holder "<<i+1<<endl;
     cout<<"Enter the account holder name ";
     cin.ignore();
 cin.getline(depositer_name,50);
@@ -25,13 +28,15 @@ cin.getline(depositer_name,50);
 cin.ignore();
 cin.getline(type_of_acc,10);
     cout<<"Enter the amount ";
-    cin>>balance_amount;
+
+      cin>>balance;
+        balance_amount.push_back(balance);
     cout<<endl;
     
-    write_detail();
+    write_detail(i);
 }
 
-void write_detail(){
+void write_detail(int &i){
 
 	ofstream file;
     string str=to_string(account_number);
@@ -39,16 +44,16 @@ void write_detail(){
     file<<"\nAccount Holder Name: "<<" "<<depositer_name;
      file<<"\nAccount Number: "<<" "<<account_number;
        file<<"\nAccount Type: "<<" "<<type_of_acc;
-         file<<"\nBalance: "<<" "<<balance_amount;     
+         file<<"\nBalance: "<<" "<<balance_amount[i];     
 file.close();
 
 }
 
-void allCustomers(){
+void allCustomers(int &i){
 	ofstream file;
    
     file.open("allcustomers.txt",ios::app);
-    file<<depositer_name<<"         "<<account_number<<"      "<<type_of_acc<<"    "<<balance_amount<<endl;    
+    file<<depositer_name<<"                           "<<account_number<<"            "<<type_of_acc<<"               "<<balance_amount[i]<<endl;    
 file.close();
 
 
@@ -56,16 +61,16 @@ file.close();
 
 void getList(){
 cout<<"\n\n\t\tACCOUNT HOLDER LIST\n\n";
-	cout<<"===========================================================-\n";
-	cout<<"Name.             A/c no              Type      Balance\n";
-	cout<<"===========================================================\n";
+	cout<<"======================================================================================-\n";
+	cout<<"Name.                                 A/c no              Type               Balance\n";
+	cout<<"======================================================================================\n";
    fstream file;
    string myfile;
    file.open("allcustomers.txt");
    while (getline (file, myfile)) {
   cout<<myfile<<endl;
    }
-    cout<<"\n--------------------------------------------------------- ";
+    cout<<"\n------------------------------------------------------------------------------------";
 
   cout<<endl;
   }
@@ -74,7 +79,7 @@ cout<<"\n\n\t\tACCOUNT HOLDER LIST\n\n";
 void deleteFile(long long int x){
   string s=to_string(x);
    if(remove(("individual_records/"+(s)+".txt").c_str())==0){
-    cout<<"File deleted successfully!!!";
+    cout<<"File deleted successfully!!!"<<endl;
 
    }
    else {
@@ -84,18 +89,56 @@ void deleteFile(long long int x){
 
 }
 
+};
+
+class transaction:public customer{
+  public:
+long double updated_balance;
+
+public:
+void deposit(int &i){
+  cout<<"Enter the account number ";
+long long int a;
+cin>>a;
+  cout<<"Enter the amount to deposit ";
+  long double amt;
+  cin>>amt;
+  updated_balance=balance_amount[i]+amt;
+  cout<<endl;
+  cout<<"Balance "<<balance_amount[i]<<endl;
+   cout<<"Updated Balance "<<updated_balance<<endl;
+
+  cout<<endl;
+   	ofstream file;
+    string str=to_string(a);
+    file.open(("individual_records/"+(str)+".txt").c_str(),ios::app);
+         file<<"\nAfter Deposting Updated Balance: "<<" "<<updated_balance<<endl;  
+file.close();
+
+}
+
+void withdraw(int &i){
+  cout<<"Enter the account number ";
+  long long int a;
+  cin>>a;
+  cout<<"Enter the amount to withdraw ";
+  long int with_draw;
+  cin>>with_draw;
+  updated_balance=balance_amount[i]-with_draw;
+    cout<<endl;
+  cout<<"Balance "<<balance_amount[i]<<endl;
+   cout<<"Updated Balance "<<updated_balance<<endl;
+  cout<<endl;
+     	ofstream file;
+    string str=to_string(a);
+    file.open(("individual_records/"+(str)+".txt").c_str(),ios::app);
+         file<<"\nAfter Withdrawing Updated Balance: "<<" "<<updated_balance<<endl;  
+file.close();
+
+}
 
 
-
-
-
-
-
-
-
-
-
-
+  
 
 
 
@@ -114,6 +157,7 @@ int x;
   cin>>x;
 customer c[x];
 customer cl;
+transaction t;
 int cas;
   do{
    
@@ -121,14 +165,15 @@ int cas;
    cout<<"Press 1 to add new customers"<<endl;
    cout<<"Press 2 to display all the details of account holders"<<endl;
    cout<<"Press 3 to delete the information related to account holder"<<endl;
+    cout<<"Press 4 to either deposit or withdraw"<<endl;
    cin>>cas;
   
    switch (cas)
    {
    case 1:
    for(int i=0;i<x;i++){
-        c[i].addCustomers();
-        c[i].allCustomers();
+        c[i].addCustomers(i);
+        c[i].allCustomers(i);
     }
     break;
    case 2:
@@ -138,21 +183,51 @@ int cas;
 
 
 case 3:
-cout<<"Enter the account number of customer ";
+
 long long int a;
+for(int i=0;i<x;i++){
+  cout<<"Enter the account number of customer "<<i+1<<endl;
 cin>>a;
 cl.deleteFile(a);
-
-
+}
 break;
 
-   case 4:
+case 4:
+cout<<"Do you want to deposit or withdraw "<<endl;
+cout<<"Press d for deposit "<<endl;
+cout<<"Press w for deposit "<<endl;
+
+for(int i=0;i<x;i++){
+cout<<endl;
+cout<<"Enter the  choice for customer "<<i+1<<endl;
+char c;
+cin>>c;
+switch (c)
+{
+  cout<<endl;
+  
+  case 'd':
+  
+ t.deposit(i);
+
+ break;
+  case 'w':
+    
+ t.withdraw(i);
+  break;
+default:
+cout<<"Invalid choice"<<endl;
+  break;
+}
+}
+
+   case 5:
    return 0;
    default:
    cout<<"none"<<endl;
    }
   }
-while (true);
+while (cas!=5);
 
 
 
